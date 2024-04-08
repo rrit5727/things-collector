99f8b3200e656ebc04a66ec92d0dd9b5200859b4
rrit5727 <rrit5727@gmail.com>
2024-04-08 23:03:32 +1000

    collector lab 2 end

--- main_app/views.py
+++ main_app/views.py
@@ -18,3 +18,7 @@ def dogs_index(request):
     return render(request, 'dogs/index.html', {
         'dogs': dogs
     })
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', {'dog': dog})
