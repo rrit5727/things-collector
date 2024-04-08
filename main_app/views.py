9f7a21d647f3f54b27575a6aa1de67f1d6c896ff
rrit5727 <rrit5727@gmail.com>
2024-04-08 21:48:29 +1000

    collector lab 1

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 dogs = [
   {'name': 'bingo', 'breed': 'jack russel', 'description': 'tenacious terrier', 'age': 3},
-  {'name': 'Woofles', 'breed': 'Cholotzinquitli', 'description': 'Charming', 'age': 2},
+  {'name': 'Woofles', 'breed': 'Choloitzcuintli', 'description': 'Charming', 'age': 2},
 ]
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def dogs_index(request):
+    return render(request, 'dogs/index.html', {
+        'dogs': dogs
+    })
