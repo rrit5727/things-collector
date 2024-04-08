3e59024683e6f09eab5dbb7dfc2ebca1d8538d59
rrit5727 <rrit5727@gmail.com>
2024-04-08 22:41:39 +1000

    collector lab 2 - 1st commit

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Dog(models.Model):
+    name = models.CharField(max_length=20)
+    breed = models.CharField(max_length=20)
+    description = models.TextField(max_length=20)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
-dogs = [
-  {'name': 'bingo', 'breed': 'jack russel', 'description': 'tenacious terrier', 'age': 3},
-  {'name': 'Woofles', 'breed': 'Choloitzcuintli', 'description': 'Charming', 'age': 2},
-]
+from .models import Dog
+
+# dogs = [
+#   {'name': 'bingo', 'breed': 'jack russel', 'description': 'tenacious terrier', 'age': 3},
+#   {'name': 'Woofles', 'breed': 'Choloitzcuintli', 'description': 'Charming', 'age': 2},
+# ]
 
 def home(request):
     return render(request, 'home.html')
@@ -12,6 +14,7 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {
         'dogs': dogs
     })
