f0a7e5a39cafd7f645236fb09bad17cd9c5217bc
rrit5727 <rrit5727@gmail.com>
2024-04-09 21:14:34 +1000

    collector lab 3

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Dog(models.Model):
@@ -10,4 +11,5 @@ class Dog(models.Model):
     def __str__(self):
         return self.name
     
-    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 from .models import Dog
 
@@ -22,3 +23,16 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', {'dog': dog})
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+    success_url = '/dogs/{dog_id}'
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs'
