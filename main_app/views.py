84f4405ddbaf4253c4370338bf987094bf9317d0
rrit5727 <rrit5727@gmail.com>
2024-04-09 23:15:21 +1000

    collector lab 4 finish

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView, UpdateView, DeleteView
 
 from .models import Dog
@@ -40,3 +40,12 @@ class DogUpdate(UpdateView):
 class DogDelete(DeleteView):
     model = Dog
     success_url = '/dogs'
+
+def add_feeding(request, dog_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = dog_id
+        new_feeding.save()
+    return redirect('detail', dog_id=dog_id)
+
